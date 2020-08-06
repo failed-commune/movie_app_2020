8045ec579da0684b96e08a5e954c90e3604ddc97
wang <w.hanseul@gmail.com>
2020-08-06 15:46:32 +0900

    componentDidMount, updeat, unmount 를 배웠음

--- src/App.js
+++ src/App.js
@@ -12,8 +12,21 @@ class App extends React.Component{
   minus = () => {
     this.setState(current => ({ count: current.count -1}))
   };
+// 최초 컴포넌트가 생성/랜더 된 후 실행되는 함수
+  componentDidMount(){
+    console.log("component rendered");
+  }
+// 셋 스테이트를 했으니, 업데이트 렌더 후 실행되는 함수
+  componentDidUpdate(){
+    console.log("I just Updated")
+  }
+// 리프레시 또는 페이지 이동 등으로 컴포넌트 언마운트를 할 때 실행되는 것이어야 하나 실행 안됨.
+  componentWillUnmount(){
+    console.log("Goobye, creul world");
+  }
 
   render(){
+    console.log("Im rendering");
     return (
     <div>
       <h1>The number is : {this.state.count}</h1>
